68c3d006883517baf18a09ea7974628e139624b0
HisashiIwamoto <hisashi.iwamoto@petabit.co.jp>
2020-06-02 17:24:37 +0900

    file

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    $text = @$_GET['text'];
+?>
+
+
 <!doctype html>
 <html lang="ja">
     <head>
@@ -6,8 +11,11 @@
 
     <body>
         <form method = 'get'>
-            <input type = "text">
+            <input type = "text" name = "text">
             <input type = "submit" value = "送信">
+            <p><?php 
+                echo $text;
+            ?></p>
         </form>
     </body>
 </html>
