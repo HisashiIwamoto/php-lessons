4ec8bad5ef78f39c1526d8680f2d793b2b8127bb
peta-hikari <hikari.minebayashi@petabit.co.jp>
2020-06-02 16:56:15 +0900

    phpの勉強

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <form method = 'get'>
+            <input type = "text">
+            <input type = "submit" value = "送信">
+        </form>
+    </body>
+</html>
